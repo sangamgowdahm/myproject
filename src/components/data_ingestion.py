54613c1ae5fdcff732aaceca94e5bcc5325818a8
sangamgowdahm <sangamgowdahm24@gmail.com>
2025-02-13 11:21:58 +0530

    final one

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -26,7 +26,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notes/StudentsPerformance.csv')
+            df=pd.read_csv('notes/StudentsPerformance1.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
